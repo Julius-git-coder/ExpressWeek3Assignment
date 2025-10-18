2441a798df5cdae95727dd5a3c00505396064ce1
Julius <daganajulius5@gmail.com>
2025-10-18 16:29:55 +0000

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import productRoutes from "./routes/productRoutes.js";
 const app = express();
 app.use(express.json());
 
-// Mount each controller on distinct routess
+// Mount each controller on distinct routesssss
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
